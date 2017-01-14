3f5933c1d5f20f2653a1025529c59d6033570167
Jared-Lee <tkigood@gmail.com>
2017-01-15 01:16:12 +0800

    暫時隱藏作廢按鈕

--- settings/client.php
+++ settings/client.php
@@ -424,7 +424,7 @@
 					<li id='list5'>
 						<div id='modify'>".$button1."</div>
 						<div id='create'>".$button2."</div>
-						<div id='invalid'>".$button3."</div>
+						<!--<div id='invalid'>".$button3."</div>-->
 					</li>
 					" ;
 				}
