e4faecf215e70bc7652bff8b6c1f43b1739efbc2
Jared-Lee <tkigood@gmail.com>
2017-01-19 16:29:47 +0800

    微調表格單雙行顏色

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?	/*秉霖初版配色	$header_bg_color = "#FF9999" ; 	$asideL_bg_color = "#5b88f6" ; 	$main_bg_color = "#FFFFCC" ; 	*/
-	/*家睿初版配色	$header_bg_color = "#472D1E" ; 	$even_rows_bg_color		= "#FFD2BF";	$even_rows_font_color	= "#280E00";	// + 秉霖二版配色 $main_bg_color	= "#FFF" ;		*/
+	/*家睿初版配色	$header_bg_color = "#472D1E" ; 	$even_rows_bg_color		= "#FFD2BF";	$even_rows_font_color	= "#280E00";	// + 秉霖二版配色 $main_bg_color= "#FFF" ;$odd_rows_color= "#FAFFFF";$even_rows_color "#FFFFFA";*/
 	
 	/*標題列配置*/
 	$header_height 			= "10%";					/*標題列高度*/
-	$header_bg_color 		= "#c9cbb1" ;				/*標題列底色*/ //078285 #c9cbb1 : #333147
+	$header_bg_color 		= "#C9CBB1" ;				/*標題列底色*/ //078285 #c9cbb1 : #333147
 	$header_font_color 		= "#333147";				/*標題文字顏色*/
 	$header_shadow_color 	= "#333147";				/*標題文字陰影顏色*/
 	$header_shadow_style 	= "0.03em 0.03em 0.2em";	/*標題文字陰影樣式*/
@@ -28,8 +28,13 @@
 	
 	$Sub_btn_activate		= "#0FA" ;					/*次選單按鈕顏色(已選中)*/
 	$Sub_btn_disable		= "#FFF" ;					/*次選單按鈕顏色(未選中)*/
-	$Sub_content_color		= "#404040" ;				/*次選單按鈕文字顏色*/
-	$Sub_content_shadow		= "#909090" ;				/*次選單按鈕文字陰影(已選中)*/
+	$Sub_content_color		= "#444" ;				/*次選單按鈕文字顏色*/
+	$Sub_content_shadow		= "#999" ;				/*次選單按鈕文字陰影(已選中)*/
+	
+	$table_border_color		= "#CCC";					/*表格邊框顏色*/
+	$table_header_color		= "#5CF";					/*表格標題底色*/
+	$odd_rows_color			= "#EFF";					/*表格奇數行底色*/
+	$even_rows_color		= "#FFE";					/*表格偶數行底色*/
 	
 	
 	/*主頁面配置*/
@@ -43,8 +48,8 @@
 	
 	$List_btn_activate		= "#3C6" ;					/*主頁面城市/清單按鈕顏色(hover)*/
 	$List_btn_disable		= "#FFF" ;					/*主頁面城市/清單按鈕顏色(一般)*/
-	$List_content_color		= "#404040" ;				/*主頁面城市/清單按鈕文字顏色*/
-	$List_border_color		= "#c0c0c0";				/*主頁面城市/清單按鈕邊框顏色*/
+	$List_content_color		= "#444" ;					/*主頁面城市/清單按鈕文字顏色*/
+	$List_border_color		= "#CCC";					/*主頁面城市/清單按鈕邊框顏色*/
 	
 	/*設定頁面配置*/	
 	$default_font_color 	= "#404040" ;				/*預設文字顏色*/
@@ -54,28 +59,22 @@
 	$input_height 			= "30px" ;					/*輸入區高度*/
 	$input_margin_top 		= "-3px" ;
 	$default_input_font_size= "16px" ;					/*輸入區字體大小*/
-	$default_active_color 	= "#28f" ;					/*輸入區顏色(啟動狀態)*/
-	$default_sleep_color 	= "#d0d0d0" ;				/*輸入區顏色(休眠狀態)*/
-	$input_bottom_bg 		= "#f5f5f5" ;				/*輸入區說明區塊底色*/
-	$input_bottom_color 	= "#c0c0c0" ;				/*輸入區說明區塊文字顏色*/
+	$default_active_color 	= "#28F" ;					/*輸入區顏色(啟動狀態)*/
+	$default_sleep_color 	= "#CCC" ;					/*輸入區顏色(休眠狀態)*/
+	$input_bottom_bg 		= "#F5F5F5" ;				/*輸入區說明區塊底色*/
+	$input_bottom_color 	= "#CCC" ;					/*輸入區說明區塊文字顏色*/
 	$input_bottom_a_color	= $default_active_color;	/*輸入區說明區塊連結顏色*/
 	
 	$Setting_Sub_btn_width	= "80%";					/*次選單按鈕寬度*/
 	$Setting_Sub_btn_height	= "14.5%";					/*次選單按鈕高度 秉霖預設100px*/
 	
-	$submit_btn_color		= "#48f";					/*提交按鈕的本體顏色*/
-	$submit_btn_shadow		= "#37f";					/*提交按鈕的陰影顏色*/
-	$submit_content_color	= "#fff";					/*提交按鈕的文字顏色*/
-	$submit_content_shadow	= "#efefef";				/*提交按鈕的文字陰影*/
+	$submit_btn_color		= "#48F";					/*提交按鈕的本體顏色*/
+	$submit_btn_shadow		= "#37F";					/*提交按鈕的陰影顏色*/
+	$submit_content_color	= "#FFF";					/*提交按鈕的文字顏色*/
+	$submit_content_shadow	= "#EFEFEF";				/*提交按鈕的文字陰影*/
 		
-	$table_border_color		= "#c0c0c0";				/*表格邊框顏色*/
-	$table_header_color		= "#5CF";					/*表格標題底色*/
-	$odd_rows_color			= "#fffffa";				/*表格奇數行底色*/
-	$even_rows_color		= "#faffff";				/*表格偶數行底色*/
-	
-	
 	/*底層配置*/
-	$footer_bg_color 		= "#c9cbb1";				/*底層底色*/
+	$footer_bg_color 		= "#C9CBB1";				/*底層底色*/
 	$footer_height 			= "3%";						/*底層高度*/
 	$footer_context 		=							/*底層內容*/
 							"最佳瀏覽解析度：1920*1080 
