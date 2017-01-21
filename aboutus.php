253e55134647df391ab63213629133d9c5724727
Jared-Lee <tkigood@gmail.com>
2017-01-21 10:28:19 +0800

    The X-file. DO NOT open.

--- aboutus.php
+++ aboutus.php
@@ -6,43 +6,6 @@
 </style>
 </head>
 <body bgcolor="#FFFFFF">
-	<div align="center"> 
-		科技公司 商業軟體<br><br>
-	</div>
-	<div align="center">
-		<h1>
-			感謝所有為這個系統付出的人！<br><br><br>
-		</h1>
-	</div>
-	<div align="center">
-		<h2>
-			召集人暨業務聯絡人：陳敬宗<br><br><br>
-		</h2>
-	</div>
-	<div align="center">
-		<h2>
-			主技術支援、主架構設計暨設定功能設計：蔡秉霖<br><br><br>
-		</h2>
-	</div>
-	<div align="center">
-		<h2>
-			資料庫設計、查詢功能設計暨專案整合：李家睿<br><br><br>
-		</h2>
-	</div>
-	<div align="center">
-		<h2>
-			創建功能設計：田偉安<br><br><br>
-		</h2>
-	</div>
-	<div align="center">
-		<h2>
-			介面美化設計：陳威誌<br><br><br>
-		</h2>
-	</div>
-	<div align="center">
-		<h2>
-			特別感謝：劉康威<br><br><br>
-		</h2>
-	</div>
+	
 </body>
 </html>
