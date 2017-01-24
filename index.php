93b1d3c36bc21f5fe1fb08deda4787839b65c4ac
eric610446 <eric610446@gmail.com>
2017-01-24 14:40:19 +0800

    1. 修改 item 介面，幣值固定為 TWD 2. 加入設定說明首頁

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
 	include 'config.php' ;
 	include 'functions.php' ;
-	include 'style.php' ;	
+	include 'style.php' ;
 ?>
 
 <html>
@@ -13,7 +13,7 @@
 	<header>
 		<?php echo $header_context; ?>
 	</header>
-	
+
 	<?php
 	//必須讓主選單的選擇優先於次選單的記憶變數，不然會被記憶變數搶走優先權
 	if(isset($_REQUEST['main_choose']))
@@ -22,7 +22,7 @@
 		$action_choose=$_REQUEST['Which_Main_choose'];
 	else
 		$action_choose=0;
-	
+
 	if( $action_choose>0)
 	{
 		$Index_Style=2;
@@ -41,57 +41,57 @@
 			//列出主選單
 			if($action_choose==1)
 				echo "	<div class='div_Main_asideR'>
-							<button type=submit name=main_choose 
+							<button type=submit name=main_choose
 									class='btn_main btn_main_active' value=1 >";
 			else
 				echo "	<div class='div_Main_aside'>
-							<button type=submit name=main_choose 
+							<button type=submit name=main_choose
 									class='btn_main btn_main_disable' value=1 >";
-									
+
 				echo "					建立<br>報價單
 							</button>
 						</div>";
 
 				if($action_choose==2)
 					echo "	<div class='div_Main_asideR'>
-								<button type=submit name=main_choose 
+								<button type=submit name=main_choose
 										class='btn_main btn_main_active' value=2 >";
 				else
 					echo "	<div class='div_Main_aside'>
-								<button type=submit name=main_choose 
+								<button type=submit name=main_choose
 										class='btn_main btn_main_disable' value=2 >";
-										
+
 					echo "					查詢<br>報價單
 								</button>
 							</div>";
-				
+
 				if($action_choose==3)
 					echo "	<div class='div_Main_asideR'>
-								<button type=submit name=main_choose 
+								<button type=submit name=main_choose
 										class='btn_main btn_main_active' value=3 >";
 				else
 					echo "	<div class='div_Main_aside'>
-								<button type=submit name=main_choose 
+								<button type=submit name=main_choose
 										class='btn_main btn_main_disable' value=3 >";
-										
+
 					echo "					查詢<br>成交訂單
 								</button>
 							</div>";
-	?>	
+	?>
 			<div class='div_Main_aside'>
-				<button type=button 
-						onclick=window.open('./settings/client.php?mode=modify') 
+				<button type=button
+						onclick=window.open('./settings/index.php')
 						class='btn_main btn_main_disable' >
 						設定<br>資料
-				</button>			
-			</div>		
-		</div>	
+				</button>
+			</div>
+		</div>
 	<?php
 		//次選單區：要存在才會出現這塊div
 		if( $action_choose>0){
 			Sub_Aside($action_choose);
 		}
-	?>		
+	?>
 	</aside>
 
 	<?php
@@ -105,18 +105,18 @@
 		//主畫面的四個 button 的動作
 		if($_POST['main_choose']==1) {
 			//main_create_quotation() ;	//偉安負責的工作項目
-			
+
 			//新版建立報價單入口
 			main_search_way(1,1) ;
 		}
 
 		if($_POST['main_choose']==2) {
 			main_search_way(2,1) ;
-		}		
+		}
 
 		if($_POST['main_choose']==3) {
 			main_search_way(3,1) ;
-		}				
+		}
 //////////////----↓ 偉安工作區域 ↓----------------------------------------------------------------------------------------------------
 		//在建立報價單，選擇了一個要建立報價單的客戶，引導到輸入產品數量單價的介面
 		if(isset($_POST['btm_confirm_client'])) {
@@ -125,7 +125,7 @@
 
 		//接收 報價單要報價的產品及數量與單價，存到每個客戶的 table
 		$quotation_arr = array() ;
-		if(isset($_POST['btm_sent_quotation_list'])) {	
+		if(isset($_POST['btm_sent_quotation_list'])) {
 			for ( $i=1 ; $i<=10 ; $i++ ) {
 				$client = $_POST['create_quotation_client'] ;
 				$product_tmp = $_POST['select_product'.$i] ;
@@ -143,20 +143,20 @@
 			}
 			save_quotation( $client, $quotation_arr) ;
 		}
-		
-		// 家睿：下面這個交給偉安寫，現在是空function					
+
+		// 家睿：下面這個交給偉安寫，現在是空function
 		/*<<<按鈕在查詢報價單中>>>，輸入值為某個訂單/報價單，可修改這個訂單的客戶、增減物品種類(ps.刪物品種類，直接把該項的invaild標記為1比較快)、更改數量*/
-		if(isset($_POST['btm_edit_quo'])) {	
+		if(isset($_POST['btm_edit_quo'])) {
 			$qu_id = $_POST['btm_edit_quo'] ;
 			edit_quotation($qu_id) ;
 		}
-		
+
 //////////////----↑ 偉安工作區域 ↑----------------------------------------------------------------------------------------------------
-		
-		
+
+
 //////////////----↓ 家睿工作區域 ↓----------------------------------------------------------------------------------------------------
-		
-		if(isset($_POST['srch_way_choose'])){			
+
+		if(isset($_POST['srch_way_choose'])){
 			main_search_way($_POST['Which_Main_choose'],0);
 		}
 
@@ -164,37 +164,37 @@
 		if(isset($_POST['btn_city_to_customer'])) {
 			echo_city_customer($_POST['btn_city_to_customer']) ;
 		}
-		
+
 		//選擇一個要查詢年分月份後，依照日期列出報價單。
 		if(isset($_POST['btn_date_to_quo'])) {
 			echo_list_month_all_simple_quo($_POST['btn_date_to_quo']);
 		}
-		
+
 		//選擇一個要查詢報價單的客戶後，依照日期列出報價單。
 		if(  isset( $_POST['btn_list_simple_quo'] )  ) {
 			echo_list_single_customer_simple_quo($_POST['btn_list_simple_quo']);
 		}
-		
+
 		//選擇要查詢客戶的其中一個報價單的詳細內容後
-		if(isset($_POST['btn_list_detail_quo'])) {	
+		if(isset($_POST['btn_list_detail_quo'])) {
 			echo_detail_quotation($_POST['btn_list_detail_quo']) ;
 		}
-				
+
 		/*列出特定物品的供應商詳細資料*/
-		if(isset($_POST['btn_detail_customer'])) {	
+		if(isset($_POST['btn_detail_customer'])) {
 			//給的是客戶id
 			list_detail_cus_or_sup_info(1,$_POST['btn_detail_customer']) ;
 		}
-		if(isset($_POST['btn_detail_supplier'])) {	
+		if(isset($_POST['btn_detail_supplier'])) {
 			//給的是物品id
 			list_detail_cus_or_sup_info(2,$_POST['btn_detail_supplier']) ;
 		}
-		
+
 		/*輸入值為某個訂單/報價單，將其is_order屬性更改為報價單/訂單，如果訂單流水號是空的，要產生新的訂單流水號*/
-		if(isset($_POST['btm_order_change'])) {	
+		if(isset($_POST['btm_order_change'])) {
 			order_change($_POST['btm_order_change']) ;
 		}
-		
+
 		//建立 pdf
 		if(isset($_POST['create_pdf'])) {
 			$client = $_POST['quotations_client'] ;
@@ -202,7 +202,7 @@
 			create_quotation_pdf( $client, $index ) ;
 		}
 
-		
+
 //////////////----↑ 家睿工作區域 ↑----------------------------------------------------------------------------------------------------
 
 	?>
---
+++ settings/index.php
@@ -0,0 +1,91 @@
+<html>
+
+<head>
+	<title>設定頁面</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	<script>
+	$(document).keypress(function(e) {
+		if(e.which == 13) {
+			return false ;
+		}
+	});
+	</script>
+</head>
+
+
+<body class='settings'>
+<?
+//----------------------------------------------------------------------------------------------
+//-									   INITIALIZE						     				   -
+//----------------------------------------------------------------------------------------------
+		require("function.php") ;
+		require("var.php") ;
+		include("initialize.php") ;
+		include("style.php") ;
+		include("../config.php") ;
+?>
+<?
+//----------------------------------------------------------------------------------------------
+//-										Context											 -
+//----------------------------------------------------------------------------------------------
+?>
+	<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+		<header>
+			<?php echo $header_context; ?>
+			<!--<h1>SIX MONSTER ACCOUNT MANAGEMENT SYSTEM</h1>
+			<h2><font face="Droid Serif"><<客戶>></font></h2>-->
+		</header>
+
+		<nav>
+			<ul>
+				<div id='client' onclick="location.href='client.php?mode=modify'"><a href='client.php?mode=modify' id='client'>客戶<br/>資料</a></div>
+				<div id='supplier' onclick="location.href='supplier.php?mode=modify'"><a href='supplier.php?mode=modify' id='supplier'>供應商<br/>資料</a></div>
+				<div id='item' onclick="location.href='item.php?mode=modify'"><a href='item.php?mode=modify' id='item'>物品<br/>清單</a></div>
+				<div id='location' onclick="location.href='location.php?mode=modify'"><a href='location.php?mode=modify' id='location'>地點<br/>資料</a></div>
+			</ul>
+		</nav>
+
+		<div id='mode'>
+		</div>
+		<div id="right">
+		</div>
+		<content id='info'>
+			<table>
+			<th>設定說明</th>
+			<tr><td>
+				<ol>建立時，請留意是否有重複名稱存在的資料，系統將會提醒您此項目已建立。</ol>
+				<ol>修改可以使用項目名稱搜尋，可搜尋部分關鍵字，在提醒使用者該項目名稱不存在後，會在右側欄顯示類似項目名稱的提示。</ol>
+				<ol>查詢功能可檢視所有已建立的項目。</ol>
+				<ol>請勿在沒有工程師協助的環境下，直接到後端 SQL 資料庫修改內容。</ol>
+			</td></tr>
+			</table>
+		</content>
+
+		<footer><?php echo $footer_context; ?></footer>
+
+
+
+
+
+	</form>
+
+
+	<script>
+	function create_duplicate() {
+		alert("建立失敗") ;
+	}
+	</script>
+
+
+</body>
+</html>
+
+
+
+<?php
+//----------------------------------------------------------------------------------------------
+//-                                         Style                                              -
+//----------------------------------------------------------------------------------------------
+include("style.php") ;
+
+?>
